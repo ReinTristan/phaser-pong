e8c65cea8af6942545c389fc47d9b138d90590ae
ReinTristan <zetaeugene@gmail.com>
2022-02-28 19:03:51 -0500

    changes to baseurl

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-export default config = {
+import { defineConfig } from "vite"
+
+export default defineConfig({
     root: 'src',
     build: {
         outDir: '../dist'
-    }
-}
+    },
+    base:'https://reintristan.github.io/phaser-pong'
+})
